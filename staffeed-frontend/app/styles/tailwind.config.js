ad12f7f40874b45997e0c1f77b94a92aa79afbcb
Tze Ming <e0311042@u.nus.edu>
2021-10-07 23:52:57 +0800

    Init side nav

--- staffeed-frontend/app/styles/tailwind.config.js
+++ staffeed-frontend/app/styles/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -11,5 +13,14 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant, e }) {
+      addVariant('active-link', ({ modifySelectors, separator }) => {
+        modifySelectors(
+          ({ className }) =>
+            `.${e(`active-link${separator}${className}`)}.active`
+        );
+      });
+    }),
+  ],
 };
