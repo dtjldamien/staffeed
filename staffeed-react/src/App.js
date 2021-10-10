45039d5b37fc8d441f2857e4aa3b4b7333db9036
Tze Ming <e0311042@u.nus.edu>
2021-10-10 14:43:46 +0800

    Init tailwind css for react

--- staffeed-react/src/App.js
+++ staffeed-react/src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-green-100">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
