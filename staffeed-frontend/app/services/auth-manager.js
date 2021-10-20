2ae5e96f96689f8551df290310bee275c8f2782e
Hendrick <henchicky@gmail.com>
2021-10-20 11:16:49 +0800

    working fetch example

--- staffeed-frontend/app/services/auth-manager.js
+++ staffeed-frontend/app/services/auth-manager.js
@@ -1,5 +1,4 @@
 import Service from '@ember/service';
-// import fetch from 'fetch';
 import axios from 'axios';
 export default class AuthManagerService extends Service {
   accessToken = null;
@@ -10,26 +9,30 @@ export default class AuthManagerService extends Service {
       password: password,
     };
 
-    //Fetch works too!!! just idk how to get token out from fetch response
-
-    // let fetchObject = {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-type': 'application/json',
-    //   },
-    //   body: JSON.stringify(userData),
-    // };
-    //  fetch('https://gara6.bg/auto-api/users/login', fetchObject).then(
+    let fetchObject = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    };
+    // fetch('https://gara6.bg/auto-api/users/login', fetchObject).then(
     //   (result) => {
     //     this.set('accessToken', result.access_token);
     //   }
     // );
-    // const data = await fetch('http://localhost:8080/auth/login', fetchObject);
+    const response = await fetch(
+      'http://localhost:8080/auth/login',
+      fetchObject
+    );
+    const data = await response.json();
+    console.log(data);
+    this.set('accessToken', data.token);
 
-    axios.post('http://localhost:8080/auth/login', userData).then((res) => {
-      console.log(res.data);
-      this.set('accessToken', res.data.token);
-    });
+    // axios.post('http://localhost:8080/auth/login', userData).then((res) => {
+    //   console.log(res.data);
+    //   this.set('accessToken', res.data.token);
+    // });
   }
 
   invalidate() {
