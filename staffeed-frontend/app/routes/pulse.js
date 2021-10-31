9f1b781dcbf6895822972db349fdf005ccadde36
Tze Ming <e0311042@u.nus.edu>
2021-10-31 19:53:50 +0800

    Init overview

--- staffeed-frontend/app/routes/pulse.js
+++ staffeed-frontend/app/routes/pulse.js
@@ -1,3 +1,18 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
 
-export default class PulseRoute extends Route {}
+export default class PulseRoute extends Route {
+  async model() {
+    const client = axios.create({
+      baseURL: 'http://localhost:8080',
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYzNTY2MTc2NiwiZXhwIjoxNjM1NzQ4MTY2fQ.UxqtUBxOGDF79oa_m7hlP0G4fveRVOKpG7P0y1EJ5VKF03nobhl78z5R2-E827ekkd2XIsHpUGLPtBBpWyTYUQ',
+      },
+    });
+
+    let { data: responseRate } = await client.get('/analytics/response-rate');
+    let { data: responses } = await client.get('/analytics/responses');
+    return { responseRate, responses };
+  }
+}
