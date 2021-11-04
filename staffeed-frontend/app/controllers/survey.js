51e2a7ee0023a4bc446a70605a2304223bbe748d
Damien Tan <dtjldamien@gmail.com>
2021-11-04 20:10:45 +0800

    feat: survey submission

--- staffeed-frontend/app/controllers/survey.js
+++ staffeed-frontend/app/controllers/survey.js
@@ -1,7 +1,20 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class SurveyController extends Controller {
+  @service('router') router;
+  @service('auth-manager') authManager;
+
+  @tracked isSubmitted = false;
+
   get questions() {
     return this.model.questions;
+  }
+
+  @action
+  submit() {
+    this.isSubmitted = true;
   }
 }
